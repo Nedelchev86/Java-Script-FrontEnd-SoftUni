c77b8d27ef267e081a47d361db186e907ebe2d7f
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-09 20:33:53 +0200

    JS Functions and Statements Exercises

--- 10_Functions and Statements-Exercises/06. Password Validator.js
+++ 10_Functions and Statements-Exercises/06. Password Validator.js
@@ -1,47 +1,13 @@
-function solve(pass) {
-  const first = lengthValidator(pass);
-  const second = onlyLetter(pass);
-  const third = leastToDigit(pass);
+function solve(password) {
+    let output = [];
 
-  //   if (lengthValidator(pass) && onlyLetter(pass) && leastToDigit(pass)) {
-  //     console.log("Password is valid");
-  //   }
-}
+    if (password.length <= 5 || password.length > 10) output.push("Password must be between 6 and 10 characters");
 
-function lengthValidator(text) {
-  if (text.length >= 6 && text.length <= 10) {
-    return true;
-  } else {
-    console.log("Password must be between 6 and 10 characters");
-  }
-  return false;
-}
+    if (!/^[a-zA-Z0-9]+$/.test(password)) output.push("Password must consist only of letters and digits");
 
-function onlyLetter(text) {
-  for (let index = 0; index < text.length; index++) {
-    let el = text[index];
-    if (el.toLowerCase() === el.toUpperCase() || !isNaN(el)) {
-      console.log("Password must consist only of letters and digits");
-      return false;
-    }
-    return true;
-  }
-}
+    if (!/\d.*\d/.test(password)) output.push("Password must have at least 2 digits");
 
-function leastToDigit(text) {
-  let count = 0;
-  for (let index = 0; index < text.length; index++) {
-    const el = text[index];
-    if (!isNaN(el)) {
-      count += 1;
-    }
-  }
-  if (count >= 2) {
-    return true;
-  } else {
-    console.log("Password must have at least 2 digits");
-    return false;
-  }
+    console.log(output.length > 0 ? `${output.join("\n")}` : "Password is valid");
 }
 
 solve("Pa$s$s");
