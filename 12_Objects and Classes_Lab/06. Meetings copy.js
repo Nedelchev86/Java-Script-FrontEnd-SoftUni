e8dde0dc9be1991104ae2a37d8c2d88ef059e00e
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-15 20:37:12 +0200

    Another solution ( hasOwnProperty )

---
+++ 12_Objects and Classes_Lab/06. Meetings copy.js
@@ -0,0 +1,19 @@
+function meeting(arr) {
+    const meeting = {};
+
+    for (const element of arr) {
+        const [key, value] = element.split(" ");
+        if (!meeting.hasOwnProperty(key)) {
+            meeting[key] = value;
+            console.log(`Scheduled for ${key}`);
+        } else {
+            console.log(`Conflict on ${key}!`);
+        }
+    }
+
+    for (const [key, value] of Object.entries(meeting)) {
+        console.log(`${key} -> ${value}`);
+    }
+}
+
+meeting(["Monday Peter", "Wednesday Bill", "Monday Tim", "Friday Tim"]);
