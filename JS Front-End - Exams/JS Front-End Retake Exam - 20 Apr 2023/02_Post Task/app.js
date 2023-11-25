56ef49c12b319abd956ebcc799feb1c3999f4e8b
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-25 15:53:14 +0200

    Added Post Button functionality

--- JS Front-End - Exams/JS Front-End Retake Exam - 20 Apr 2023/02_Post Task/app.js
+++ JS Front-End - Exams/JS Front-End Retake Exam - 20 Apr 2023/02_Post Task/app.js
@@ -1,5 +1,58 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  
+    const allInputFields = {
+        taskTitle: document.querySelector("#task-title"),
+        taskCategory: document.querySelector("#task-category"),
+        taskContent: document.querySelector("#task-content"),
+    };
+
+    const postValue = {
+        taskTitle: null,
+        taskCategory: null,
+        taskContent: null,
+    }
+
+    function createDomElement(type, parrent, textContent, classList, id, attributes, useInnerHtml) {
+        const element = document.createElement(type);
+
+        if (useInnerHtml && textContent) {
+            element.innerHTML = textContent;
+        } else {
+            if (textContent && type !== "input") {
+                element.textContent = textContent;
+            }
+        }
+        if (textContent && type === "input") {
+            element.value = textContent;
+        }
+
+        if (classList && classList.length > 0) {
+            element.classList.add(...classList);
+        }
+
+        if (id) {
+            element.id = id;
+        }
+
+        if (attributes) {
+            for (const key in attributes) {
+                element[key] = attributes[key];
+            }
+        }
+
+        if (parrent) {
+            parrent.appendChild(element);
+        }
+        return element;
+    }
+
+    const publishBtn = document.querySelector("#publish-btn")
+    publishBtn.addEventListener("click", PublishPost)
+
+    function PublishPost(){
+        const {taskTitle, taskCategory, taskContent} = allInputFields
+
+
+    }
 }
