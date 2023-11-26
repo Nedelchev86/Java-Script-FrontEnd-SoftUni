43c1190fbea7cd0a42900bbf54371183070afd56
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-26 08:10:39 +0200

    Music app ( JS DOM )

--- JS Front-End - Exams/Exam Preparation II/02_Music Site/app.js
+++ JS Front-End - Exams/Exam Preparation II/02_Music Site/app.js
@@ -12,7 +12,8 @@ function solve() {
     const addBtn = document.querySelector("#add-btn");
     addBtn.addEventListener("click", addSongFunction);
     const allHitsContainer = document.querySelector(".all-hits-container");
-
+    const savedHits = document.querySelector(".saved-container");
+    const likes = document.querySelector(".likes").querySelector("p");
     function createDomElement(type, parrent, textContent, classList, id, attributes, useInnerHtml) {
         const element = document.createElement(type);
 
@@ -53,13 +54,40 @@ function solve() {
             return;
         }
 
-        const {genre, name, author, date} = allHitsContainer;
+        const {genre, name, author, date} = allInputFields;
 
         const div = createDomElement("div", allHitsContainer, null, ["hits-info"]);
-        createDomElement("img", allHitsContainer, "./static/img/img.png", null, null, "scr");
-        createDomElement("h2", allHitsContainer, `Genre: ${genre.value}`);
-        createDomElement("h2", allHitsContainer, `Name: ${name.value}`);
-        createDomElement("h2", allHitsContainer, `Author: ${author.value}`);
-        createDomElement("h3", allHitsContainer, `Date: ${date.value}`);
+        createDomElement("img", div, null, null, null, {src: "./static/img/img.png"});
+        createDomElement("h2", div, `Genre: ${genre.value}`);
+        createDomElement("h2", div, `Name: ${name.value}`);
+        createDomElement("h2", div, `Author: ${author.value}`);
+        createDomElement("h3", div, `Date: ${date.value}`);
+        const saveBtn = createDomElement("button", div, "Save song", ["save-btn"]);
+        saveBtn.addEventListener("click", saveSongFunc);
+        const likeBtn = createDomElement("button", div, "Like song", ["like-btn"]);
+        likeBtn.addEventListener("click", likeSongFunc);
+        const deleteBtn = createDomElement("button", div, "Delete", ["delete-btn"]);
+        deleteBtn.addEventListener("click", deleteSongFunc);
+        form.reset();
+    }
+
+    function likeSongFunc(e) {
+        console.log(e.currentTarget);
+        e.currentTarget.disabled = true;
+        const currentLikes = Number(likes.textContent.split(": ")[1]);
+        likes.textContent = `Total Likes: ${currentLikes + 1}`;
+        // document.querySelector(".like-btn").disabled = true;
+    }
+
+    function saveSongFunc() {
+        const currentSong = document.querySelector(".hits-info");
+        currentSong.querySelector("button").remove();
+        currentSong.querySelector("button").remove();
+        savedHits.appendChild(currentSong);
+    }
+
+    function deleteSongFunc(e) {
+        const element = e.currentTarget.parentElement;
+        element.remove();
     }
 }
