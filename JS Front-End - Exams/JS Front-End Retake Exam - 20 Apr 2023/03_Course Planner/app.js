85f12d85612e3e9dcdb6fada1525c32b89012e65
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-26 14:28:49 +0200

    AJAX and DOM manipulations

--- JS Front-End - Exams/JS Front-End Retake Exam - 20 Apr 2023/03_Course Planner/app.js
+++ JS Front-End - Exams/JS Front-End Retake Exam - 20 Apr 2023/03_Course Planner/app.js
@@ -3,16 +3,23 @@ const BASE_RUL = "http://localhost:3030/jsonstore/tasks/";
 const allInputFields = {
     title: document.querySelector("#course-name"),
     type: document.querySelector("#course-type"),
-    description: document.querySelector("#description-name"),
+    description: document.querySelector("#description"),
     teacher: document.querySelector("#teacher-name"),
 };
 
-const BtnLoadCourse = document.querySelector("#load-course");
-BtnLoadCourse.addEventListener("click", loadCourses);
-const divList = document.querySelector("#teacher-name");
+const loadCourseBtn = document.querySelector("#load-course");
+loadCourseBtn.addEventListener("click", loadCourses);
+const addCourseBtn = document.querySelector("#add-course");
+addCourseBtn.addEventListener("click", addCourse);
 
+const editCourseBtn = document.querySelector("#edit-course");
+editCourseBtn.addEventListener("click", editAndSave);
 
-let courses = [] 
+const divList = document.querySelector("#list");
+const form = document.querySelector("form");
+
+let courses = [];
+let currentCourse = "";
 
 function createDomElement(type, parrent, textContent, classList, id, attributes, useInnerHtml) {
     const element = document.createElement(type);
@@ -49,6 +56,72 @@ function createDomElement(type, parrent, textContent, classList, id, attributes,
 }
 
 async function loadCourses() {
+    divList.innerHTML = "";
     const result = Object.values(await (await fetch(BASE_RUL)).json());
-    courses = result
+    courses = result;
+
+    result.forEach((el) => {
+        const container = createDomElement("div", divList, null, ["container"], el._id);
+        createDomElement("h2", container, el.name);
+        createDomElement("h3", container, el.teacher);
+        createDomElement("h3", container, el.type);
+        createDomElement("h4", container, el.description);
+        const editBtn = createDomElement("button", container, "Edit Course", ["edit-btn"]);
+        editBtn.addEventListener("click", editLoad);
+        const finishBtn = createDomElement("button", container, "Finish Course", ["finish-btn"]);
+        finishBtn.addEventListener("click", deleteCourse);
+    });
+}
+
+async function addCourse(e) {
+    e.preventDefault();
+
+    if (Object.values(allInputFields).some((x) => x.value === "")) {
+        return;
+    }
+    const {title, type, description, teacher} = allInputFields;
+    await fetch(BASE_RUL, {
+        method: "POST",
+        body: JSON.stringify({title: title.value, type: type.value, description: description.value, teacher: teacher.value}),
+    });
+    form.reset();
+    loadCourses();
+}
+
+function editLoad() {
+    const courseId = this.parentElement.id;
+    const course = courses.find((c) => c._id === courseId);
+    currentCourse = course;
+
+    for (const key in allInputFields) {
+        allInputFields[key].value = course[key];
+    }
+    addCourseBtn.disabled = true;
+    editCourseBtn.disabled = false;
+    document.getElementById(courseId).remove();
+}
+
+async function editAndSave(e) {
+    e.preventDefault();
+    const {title, type, description, teacher} = allInputFields;
+
+    await fetch(`${BASE_RUL}${currentCourse._id}`, {
+        method: "PUT",
+        body: JSON.stringify({title: title.value, type: type.value, description: description.value, teacher: teacher.value, _id: currentCourse._id}),
+    });
+    addCourseBtn.disabled = false;
+    editCourseBtn.disabled = true;
+    form.reset();
+    loadCourses();
+}
+
+async function deleteCourse(e) {
+    const courseId = this.parentElement.id;
+
+    const course = courses.find((c) => c._id === courseId);
+
+    await fetch(`${BASE_RUL}${course._id}`, {
+        method: "DELETE",
+    });
+    loadCourses();
 }
